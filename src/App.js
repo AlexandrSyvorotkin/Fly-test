23dba7a8d0e8e8c11fa58f0414828d51b020b5dc
Alex <Alex031994@gmail.com>
2021-11-04 12:19:55 +0300

    create data query to db.json

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import './App.css';
+import {useEffect} from "react";
+import axios from "axios";
 
 function App() {
+
+    const url = 'http://localhost:3001/result'
+
+    useEffect(() => {
+        axios.get(url)
+            .then(({data}) => {
+                console.log(data)
+            })
+
+    }, [])
+
   return (
     <div className="App">
       Start
